4a497f477b851e92c5456cb8e67c7e0ab0948d9f
teberizm <151469761+teberizm@users.noreply.github.com>
2025-08-24 19:24:27 +0300

    fix: handle dynamic leaflet import

--- components/leaflet-map.tsx
+++ components/leaflet-map.tsx
@@ -7,20 +7,18 @@ export default function LeafletMap() {
 
   useEffect(() => {
     let map: any
-    const init = async () => {
-      const L = (await import("leaflet")).default
+    ;(async () => {
+      const leaflet = await import("leaflet")
       if (!mapRef.current) return
-      map = L.map(mapRef.current).setView([0, 0], 2)
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      }).addTo(map)
-    }
-    init()
-    return () => {
-      if (map) {
-        map.remove()
-      }
-    }
+      map = leaflet.map(mapRef.current).setView([0, 0], 2)
+      leaflet
+        .tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+        })
+        .addTo(map)
+    })()
+    return () => map?.remove()
   }, [])
 
   return <div ref={mapRef} className="h-64 w-full" />
